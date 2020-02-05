12c27ba96fbe53c93af47b6a659a49c3c764e94b
account931 <account931@ukr.net>
2020-02-05 14:20:11 +0200

    Add files via upload

--- js/dropAdvertise_zzz.js
+++ js/dropAdvertise_zzz.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 	//drop div with money donut
 	$('div:contains("Ця сторінка розміщена безкоштовно ")').css('display', 'none');  //css('background-color', 'red')   //drop russ version
 	$('div:contains("is hosted for free by zzz.com.ua, if you are owner of this page")').css('display', 'none');  //drop engl version
+	$('div:contains("Эта страница размещена бесплатно на")').css('display', 'none');  //drop ru version
 	//$('div:contains("Join WEB Revolution")').css('background-color', 'red'); 
 	
 	//droping Mint.me banner
@@ -19,7 +20,7 @@ $(document).ready(function(){
 			 //alert(a_href);  //show  https://www.mintme.com/
 			 
 		     if( a_href == "https://www.mintme.com/"){  //https://www.mintme.com/   https://a5.cba.pl/mintme.png
-				 alert("f y");
+				 //alert("f y");
 				 $(this).find('div').css('display', 'none');
 			 }
 		     
